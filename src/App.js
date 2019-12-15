994fabdcc608046fd439b3e65947238e0f52779c
guruprasadlakkond <guruprasadlakkond@gmail.com>
2019-12-16 00:50:26 +0530

    Intial season app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+/* export default function App(){
+    window.navigator.geolocation.getCurrentPosition((p) => console.log(p),
+    er => console.log(er));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hi, Welcome new app</h1>
     </div>
   );
-}
+}; */
 
-export default App;
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lat: 0.0,
+      err: null
+    };
+  }
+
+  componentDidMount() {
+    console.log("Comp mounted now");
+    window.navigator.geolocation.getCurrentPosition(
+      p => {
+        console.log(p);
+        this.setState({ lat: p.coords.latitude.toFixed(3) });
+      },
+      er => {
+        this.setState({ err: er.message });
+      }
+    );
+  }
+
+  componentDidUpdate() {
+    console.log("Comp updated and rerendered");
+  }
+
+  handle = () => {
+    console.log("Click handler");
+    this.setState({ lat: 37.88 });
+  };
+
+  componentWillMount() {
+    console.log("comp will mount");
+  }
+
+  render() {
+    console.log("rendered");
+    /* window.navigator.geolocation.getCurrentPosition(
+      p => {
+          console.log(p);
+        this.setState({ lat: Math.floor(p.coords.latitude) });
+      },
+      er => console.log(er)
+    ); */
+    return (
+      <div>
+        <h1>Hi, Welcome new app</h1>
+        <h2>
+          Latitude: {this.state.err !== null ? this.state.err : this.state.lat}
+        </h2>
+        <button onClick={this.handle}>Click on me!</button>
+      </div>
+    );
+  }
+}
---
+++ src/components/SeasonDisplay.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+export default class SeasonDisplay extends React.Component{
+
+    render () {
+        return (
+            <div>
+                SeasonDisplay
+            </div>
+        )
+    }
+}
